cf3f5593d64aae75f700e75d61b851e5f5d5aa94
JuniorBaymax <cooladitya953@gmail.com>
2020-02-02 10:38:01 +0530

    Updating project to webpack or Bundle.js

---
+++ dist/js/Scroll.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.feature__text',{delay: 1000});
+ScrollReveal().reveal('.check',{delay: 500});
+ScrollReveal().reveal('.gallery__item',{delay: 1000, interval: 16, reset: true });
---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+const cardColor = document.querySelectorAll(".feature__card_icon");
+console.log(cardColor);
